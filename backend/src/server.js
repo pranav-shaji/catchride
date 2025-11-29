d49b4929a4273a1fdf436add536f21faf8ffe5a8
pranav <pranavshaji2244@gmail.com>
2025-11-29 17:05:20 +0530

    fix my server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,9 +5,7 @@ import connectDB from "./config/db.js"
 connectDB()     
 
 
-const port = 5000
-
-app.listen(port,()=>{
-    console.log("server running on port",port);
-    
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
