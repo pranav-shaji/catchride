d5ece863a18f3fac2d5543ddbbe2edabf77d3ac8
pranav <pranavshaji2244@gmail.com>
2025-12-01 13:55:54 +0530

    fix dotenv overide for railway

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 import dotenv from "dotenv"
-dotenv.config()
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
+
 import app from "./app.js"
 import connectDB from "./config/db.js" 
 connectDB()     
